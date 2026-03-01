7820840eda28a42a06f6ab0350a53e19563ac59f
mohammedmustafa1427 <mohammedmustafa1427@gmail.com>
2026-03-01 22:22:20 +0000

    task 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/*
+ * main - entry point
+ *
+ * Discription: print _putchar with new line
+ *
+ * Return to 0
+ */
+
+int main(void)
+{
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+
+	return (0);
+}
