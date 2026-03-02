6fbc7438e79996e6028b5e177d5dd0f8462e944b
mohammedmustafa1427 <mohammedmustafa1427@gmail.com>
2026-03-02 22:45:39 +0300

    task 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+/**
+ * _isdigit - checks for digit character
+ * @c: the character to be checked
+ * 
+ * Return: 1 if c is digit, 0 otherwise
+*/
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return(0);
+	}
+	else
+	{
+		return(0);
+	}
+}
