45f17cbea735518d10ce6b694b5c73c679e7acfb
mohammedmustafa1427 <mohammedmustafa1427@gmail.com>
2026-03-02 23:23:10 +0300

    fix

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,13 @@
 /**
+ * print_numbers - print numbers from 0 to 9
  *
+ * Return: void
  */
 void print_numbers(void)
 {
+	int i;
 
-	for (int i = '0'; i <= '9'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		_putchar(i);
 	}
