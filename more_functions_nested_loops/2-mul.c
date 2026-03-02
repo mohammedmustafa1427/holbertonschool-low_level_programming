c01b004d361256efa229f9ebb13807fdd3ad7d46
mohammedmustafa1427 <mohammedmustafa1427@gmail.com>
2026-03-02 23:02:40 +0300

    task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+ /**
+  * _mul - multiplies to integers
+  * @a: first interger
+  * @b: second integer
+  * 
+  * Return: the result of multiplication
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
