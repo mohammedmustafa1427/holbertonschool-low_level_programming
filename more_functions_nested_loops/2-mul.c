17e7443af9f532dca2d429878dba17e014e77fb3
mohammedmustafa1427 <mohammedmustafa1427@gmail.com>
2026-03-02 23:04:24 +0300

    fix

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,10 +1,10 @@
- /**
-  * _mul - multiplies to integers
-  * @a: first interger
-  * @b: second integer
-  * 
-  * Return: the result of multiplication
-  */
+/**
+ * _mul - multiplies to integers
+ * @a: first interger
+ * @b: second integer
+ *
+ * Return: the result of multiplication
+ */
 int mul(int a, int b)
 {
 	return (a * b);
