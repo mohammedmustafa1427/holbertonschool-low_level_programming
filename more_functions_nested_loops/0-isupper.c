622e9c710a3f28a66d4a372e888e5c4cb05d81b7
mohammedmustafa1427 <mohammedmustafa1427@gmail.com>
2026-03-02 22:13:18 +0300

    task 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+
+
+/*
+* -isupper - checks for uppercase character
+* @c: the character to be checked
+*
+* return: 1 if c it uppercase ,0 otherwise
+*/
+int _isupper(int c)
+{
+    if (c>= 'A' && c<='Z')
+    {
+        return(1);
+    }
+    else
+    {
+        return(0);
+    }
+}
