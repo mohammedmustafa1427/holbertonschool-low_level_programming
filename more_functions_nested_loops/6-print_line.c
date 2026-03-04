dcfad4e80bc9a54e77a7b76dba544090ee6ddc62
mohammedmustafa1427 <mohammedmustafa1427@gmail.com>
2026-03-04 20:29:59 +0300

    task  6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line - drow a stright line in the terminal
+ * @n: the number of time character _ should be printed
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+	if (n > 0)
+	{
+		for (i > 0; n > i; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
