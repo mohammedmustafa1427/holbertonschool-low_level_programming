2a2f849c5b74fbc1eadfdc798e390b008e5e319c
mohammedmustafa1427 <mohammedmustafa1427@gmail.com>
2026-03-06 00:06:56 +0300

    fix

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -2,20 +2,28 @@
 
 /**
  * print_diagonal - drow a diagonal line in the terminal
- * @n: the number of time character _ should be printed
+ * @n: the number of time character \ should be printed
  *
  * Return: void
  */
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
 		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
 			_putchar('\\');
+			_putchar('\n');
 		}
 	}
-	_putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
 }
