c10dd64c78ad1ee85a6e1203c8c20c93c2147ba6
mohammedmustafa1427 <mohammedmustafa1427@gmail.com>
2026-03-07 22:35:21 +0300

    task 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: input string
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int full_len = 0;
+	int start;
+
+	while (str[full_len] != '\0')
+	{
+		full_len++;
+	}
+
+	if (full_len % 2 == 0)
+	{
+		start = full_len / 2;
+	}
+	else
+	{
+		start = (full_len + 1) / 2;
+	}
+
+	while (str[start] != '\0')
+	{
+		_putchar(str[start]);
+		start++;
+	}
+	_putchar('\n');
+}
+	
