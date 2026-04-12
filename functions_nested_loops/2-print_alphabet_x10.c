731a8e42ed50e43804114ce6cafe80df0facbb5b
mohammedmustafa1427 <mohammedmustafa1427@gmail.com>
2026-04-13 00:39:49 +0300

    task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * Description: print alphabet 10 times
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10c(void)
+{
+
+	char i;
+	for (i = 0; i <= 10; i++)
+	{
+		char letters;
+		for (letters = 'a'; letters < 'z'; letters++)
+		{
+			_putchar(letters);
+		}
+
+		_putchar('\n');
+	}
+}
