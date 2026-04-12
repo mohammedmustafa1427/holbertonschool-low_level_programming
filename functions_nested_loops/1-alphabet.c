4cc956d4eff845eff2950ba435440d3677bd7d8a
mohammedmustafa1427 <mohammedmustafa1427@gmail.com>
2026-04-12 22:56:56 +0300

    fix

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -10,9 +10,10 @@
 
 void print_alphabet(void)
 {
-    for (char i = 'a', i <= 'z', i++)
+    char i;
+    for (i = 'a'; i <= 'z'; i++)
     {
-        _putchar('%d', i);
+        _putchar(i);
     }
     _putchar('\n');
     return 0;
