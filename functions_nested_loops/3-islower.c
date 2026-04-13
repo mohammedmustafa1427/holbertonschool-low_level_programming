709a70031ce850801666bc86fb8806417c652c0c
mohammedmustafa1427 <mohammedmustafa1427@gmail.com>
2026-04-13 17:56:01 +0300

    fix

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * _islower - checks the lowercase character
- *
+ * _islower - checks for lowercase character
  * @c: the character to check
- * return: 1 if lower case, 0 otherwise
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
  */
-
 int _islower(int c)
 {
-
+	
 	if (c >= 'a' && c <= 'z')
 	{
-		return 0;
+		return (1);
 	}
 	else
 	{
-		return 1;
+		return (0);
 	}
 }
