aacc3928c3edf2e702541261d6542a078d9fc82a
mohammedmustafa1427 <mohammedmustafa1427@gmail.com>
2026-04-13 19:11:51 +0300

    task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_sign - check the number
+ * @n: the character to check
+ *
+ * Return: 1 if n is greater than 0, 0 if n is 0, -1 if n lower than 0
+ */
+int _abs(int)
+{
+	int n;
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-1 * n);
+	}
+}
