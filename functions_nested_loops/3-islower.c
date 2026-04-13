f5121d73d1e978998a12548acc8f8506b864771b
mohammedmustafa1427 <mohammedmustafa1427@gmail.com>
2026-04-13 17:52:55 +0300

    task 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - checks the lowercase character
+ *
+ * @c: the character to check
+ * return: 1 if lower case, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c > 'a' && c < 'z')
+	{
+		return 0;
+	}
+	else
+	{
+		return 1;
+	}
+}
