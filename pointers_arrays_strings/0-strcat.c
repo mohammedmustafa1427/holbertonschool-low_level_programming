d38eda1937e2ca668d04db4940245620fbe3a2d3
mohammedmustafa1427 <mohammedmustafa1427@gmail.com>
2026-04-14 19:44:30 +0300

    task 0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,2 +1,25 @@
 #include "main.h"
-#include <stdio.h>
+/**
+ *_strcar -  concatenates two strings.
+ 8@dest: the main character that concatenates with the sec string
+ *@src: the sec character that concatenates with main string
+
+ return: @dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
