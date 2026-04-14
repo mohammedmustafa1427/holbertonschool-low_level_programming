3a2b21d888368cc549e735bbd8d2d86e0af6d192
mohammedmustafa1427 <mohammedmustafa1427@gmail.com>
2026-04-14 20:33:27 +0300

    task 2

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -16,7 +16,7 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
-
+char *_strncpy(char *dest, char *src, int n);
 
 
 
