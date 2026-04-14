7ced72630c0416826481bef63a15185175713d02
mohammedmustafa1427 <mohammedmustafa1427@gmail.com>
2026-04-14 20:06:27 +0300

    task 1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings useing at most n byets from src
+ * @dest: the destination string
+ * @src: the source string
+ * @n: the maximum amount of bytes to copy
+ *
+ * Return: a pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -15,7 +15,7 @@ int _atoi(char *s);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strcat(char *dest, char *src);
-
+char *_strncat(char *dest, char *src, int n);
 
 
 
