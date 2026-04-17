582760031cea15208e826bc2405670a9d899f64e
mohammedmustafa1427 <mohammedmustafa1427@gmail.com>
2026-04-17 21:26:17 +0300

    s

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -17,7 +17,7 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
-
+int _strcmp(char *s1, char *s2);
 
 
 
