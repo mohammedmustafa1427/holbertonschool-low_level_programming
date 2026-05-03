435eab627e87535b0feebb75c34108dd80e67228
mohammedmustafa1427 <mohammedmustafa1427@gmail.com>
2026-05-03 20:43:33 +0300

    task 5

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -25,7 +25,7 @@ char *leet(char *);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
-
+char *_strstr(char *haystack, char *needle);
 
 
 
