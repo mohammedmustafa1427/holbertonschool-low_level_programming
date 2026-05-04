d524dd0b04d35a7e194afd8a6338f8b5db0e6560
mohammedmustafa1427 <mohammedmustafa1427@gmail.com>
2026-05-04 20:07:56 +0300

    fix

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,16 +1,32 @@
 #include "main.h"
 
+int find_sqrt(int n, int i);
+
 /**
- * factorial - returns the factorial of a given number.
- * @n: the number to calculate the factorial of.
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: number to calculate the square root of.
  *
- * Return: factorial of n, or -1 if n is lower than 0.
+ * Return: the natural square root, or -1 if it doesn't exist.
  */
-int factorial(int n)
+int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	if (n == 0)
-		return (1);
-	return (n * factorial(n - 1));
+	return (find_sqrt(n, 0));
+}
+
+/**
+ * find_sqrt - recurses to find the natural square root of a number.
+ * @n: number to calculate the square root of.
+ * @i: iterator.
+ *
+ * Return: the resulting square root.
+ */
+int find_sqrt(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (find_sqrt(n, i + 1));
 }
